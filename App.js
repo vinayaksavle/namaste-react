ef2b2e7b1a13bd06608ed41708101d0254ce5940
Vinayak Savale <vinayak.savle@gmail.com>
2025-02-13 21:59:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("div", { id: "parent" }, 
+    React.createElement("div", { id: "child" }, 
+        [React.createElement("h1", {},"h1 tag"), React.createElement("h2", {},"h2 tag")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
