6562e52ab3b363aac0034e796cb99984a82b53b6
Vinayak Savale <vinayak.savle@gmail.com>
2025-02-22 21:04:14 +0530

    main Igniting our App

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "parent" }, 
     React.createElement("div", { id: "child" }, 
-        [React.createElement("h1", {},"h1 tag"), React.createElement("h2", {},"h2 tag")]));
+        [React.createElement("h1", {},"h1 tag 1"), React.createElement("h2", {},"h2 tag")]));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
